1e0ddf8b808976fd008fc349727df1799b26cea1
ayrton-hbtn <3282@holbertonschool.com>
2022-05-02 19:52:36 -0300

    pycodestyle

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -7,8 +7,8 @@ def update_topics(mongo_collection, name, topics):
     changes all topics of a school
     document based on the name
     """
-    schools_to_update = { 'name': name }
-    new_values = { '$set': { 'topics': topics } }
+    schools_to_update = {'name': name}
+    new_values = {'$set': {'topics': topics}}
     mongo_collection.update_many(
         schools_to_update,
         new_values
