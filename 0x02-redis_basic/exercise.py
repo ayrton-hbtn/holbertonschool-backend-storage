6f4440fb70dec0806d30539a0c048c9b348f34a6
ayrton-hbtn <3282@holbertonschool.com>
2022-05-04 20:31:27 -0300

    alter get() to convert byte string back to python primitive types

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -18,3 +18,29 @@ class Cache:
         id = str(uuid.uuid4)
         self._redis.set(id, data)
         return id
+
+    def get(self, key: str, fn: Callable = None):
+        """
+        take a key string argument and an optional
+        Callable argument named fn that will be used
+        to convert the data back to the desired format.
+
+        Conserve original get behavior if key doesn't exists
+        """
+        val = self._redis.get(key)
+        if fn is not None:
+            return fn(val)
+        return val
+
+    def get_str(self, key: str) -> str:
+        """parametrize Cache.get to str"""
+        val = self._redis.get(key)
+        return val.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """parametrize Cache.get to int"""
+        try:
+            val = int(self._redis.get(key))
+        except ValueError:
+            val = 0
+        return val
